ecfeea39d6409b593c2a77ee81a9ef939233a056
pier <pierluigi.tonegato@gmail.com>
2019-05-29 02:02:49 +0200

    aggiunti  commenti

--- leggiDaInflux.py
+++ leggiDaInflux.py
@@ -1,12 +1,17 @@
+#importo le librerie: per installare  la libreria influx usare:
+#pip install influxdb
+
 from influxdb import DataFrameClient
 import matplotlib.pyplot as plt
+#imposto un dataframe e ci eseguo la query
 client = DataFrameClient('mioserver.com', 8086, 'utente', 'password', 'nomedb')
-
 result = client.query("""SELECT mean(value) FROM "cpu_load_short" 
                           WHERE time >= now() 
                           AND time < now() -1d
                           GROUP BY time(15s) fill(null)""")
+#controllo di avere dati (deve esistere la misura selezionata)
 if 'cpu_load_short' in result:
+    #estraggo i dati in un array e li grafico
     y = result['cpu_load_short'].values
     plt.title("cpu_load_short")
     plt.plot(y)
